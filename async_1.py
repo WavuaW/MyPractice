e7211a54ca143c9317c0d040d9e377dd3b5dba15
WavuaW <wavuamuroka@gmail.com>
2023-05-03 15:15:02 +0300

    List comprehensions

--- async_1.py
+++ async_1.py
@@ -1,6 +1,7 @@
 import asyncio
 
 async def fetch_data():
+    #you can only use await inside of an async function/inside an actual co routine.
     print('Start fetching')
     await asyncio.sleep(2)
     print('Done fetching')
@@ -13,11 +14,14 @@ async def print_numbers():
 
 async def main():
     #when you create a task and a coroutine returns a value this is called a Future(similar to promise in javascript)
+    #these tasks subclass the 'Future'
     task1 = asyncio.create_task(fetch_data())
     task2 = asyncio.create_task(print_numbers())
     
     #if you want the value returned form a coroutine you must await that coroutine/task
     value = await task1
+    print(value)
+    await task2
 
 
 asyncio.run(main())
