51c00f6808a3e9b1cbc949156140cf4fc05214c6
WavuaW <wavuamuroka@gmail.com>
2023-05-11 12:20:17 +0300

    Redis changes

--- Python-Practice/Redis/cache.py
+++ Python-Practice/Redis/cache.py
@@ -1,13 +1,33 @@
 import redis
+import time
 
 r = redis.Redis(host='localhost', port=6379)
 
-r.set("France", "Paris")
-r.set("Germany", "Berlin")
+# r.set("France", "Paris")
+# r.set("Germany", "Berlin")
 
-france_capital = r.get("France")
-germany_capital = r.get("Germany")
+# france_capital = r.get("France")
+# germany_capital = r.get("Germany")
 
-print(france_capital)
-print(germany_capital)
+# print(france_capital)
+# print(germany_capital)
 
+#alternatively you can do a multiple set.
+
+r.mset({"Germany": "Berlin", "France": "Paris"})
+
+print(r.get ("Germany"))
+print(r.get ("France"))
+
+if (r.exists("Germany")):
+    print(r.get("Germany"))
+else:
+    print("Cannot find the capital. Get from API")
+
+r.psetex("Serbia", 1000, "Belgrade")
+
+print(r.get("Serbia"))
+
+time.sleep(2)
+
+print(r.get("Serbia"))
