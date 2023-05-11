28b1ae2c5698eff73c7755a0813cd31201289b52
WavuaW <wavuamuroka@gmail.com>
2023-05-11 11:04:51 +0300

    redis changes

--- Python-Practice/NoSQLMonogDB/Test/src/snake_bnb/src/data/bookings.py
+++ Python-Practice/NoSQLMonogDB/Test/src/snake_bnb/src/data/bookings.py
@@ -1,6 +1,6 @@
 import mongoengine
 
-
+#this means that the document has to be a subset of something and that this information in itself cannot be a top level document/can be connected in another socument
 class Booking(mongoengine.EmbeddedDocument):
     guest_owner_id = mongoengine.ObjectIdField()
     guest_snake_id = mongoengine.ObjectIdField()
--- Python-Practice/NoSQLMonogDB/Test/src/snake_bnb/src/data/snake1.py
+++ Python-Practice/NoSQLMonogDB/Test/src/snake_bnb/src/data/snake1.py
@@ -1,12 +1,13 @@
+import datetime
 import mongoengine
 
-class Snake:
-    registered_date = mongoengine.DateTimeField()
-    species = mongoengine.StringField()
+class Snake(mongoengine.Document):
+    registered_date = mongoengine.DateTimeField(default=datetime.datetime.now)
+    species = mongoengine.StringField(required=True)
 
-    length = mongoengine.FloatField()
-    name = mongoengine.StringField()
-    is_venomous = mongoengine.BooleanField()
+    length = mongoengine.FloatField(required=True)
+    name = mongoengine.StringField(required=True)
+    is_venomous = mongoengine.BooleanField(required=True)
 
     meta = {
         'db_alias': 'core',
