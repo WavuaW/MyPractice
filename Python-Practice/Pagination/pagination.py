75dc5d0351daa2a7b6ee9ebfb89b489e75e3d7ce
WavuaW <wavuamuroka@gmail.com>
2023-05-15 18:44:59 +0300

    rick&morty api

--- Python-Practice/Pagination/pagination.py
+++ Python-Practice/Pagination/pagination.py
@@ -6,4 +6,12 @@ endpoint = 'character'
 
 r = requests.get(baseurl + endpoint)
 
-print(r.json())
+data = r.json()
+
+pages = data['info']['pages']
+
+name = data['results'][0]['name']
+
+episodes = data['results'][0]['episode']
+
+print(len(episodes))
