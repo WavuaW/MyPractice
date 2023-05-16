ab89231c6c05093f1154ce0c89ba56991a17d119
WavuaW <wavuamuroka@gmail.com>
2023-05-16 17:17:40 +0300

    redis python client

--- Python-Practice/Redis/redis_examples.py
+++ Python-Practice/Redis/redis_examples.py
@@ -1,4 +1,7 @@
 import redis
 
-client = redis.Redis(host='localhost', port=6379, db=0)
+redis_client = redis.Redis(host='localhost', port=6379, db=0)
 
+mfd = redis_client.get('msft_fundamental_data')
+
+print(mfd)
