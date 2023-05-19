1ab0f98a43220dcea2d5f527365f0f1dbb6cb9e6
WavuaW <wavuamuroka@gmail.com>
2023-05-19 21:35:27 +0300

    redis python client

--- Python-Practice/Redis/redis_examples.py
+++ Python-Practice/Redis/redis_examples.py
@@ -1,7 +1,10 @@
 import redis
+import json
 
 redis_client = redis.Redis(host='localhost', port=6379, db=0)
 
 mfd = redis_client.get('msft_fundamental_data')
 
 print(mfd)
+
+redis_client.set('microsoft_fundamental_data', json.dumps({"pe": 33}))
