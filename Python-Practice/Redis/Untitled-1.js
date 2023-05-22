587ada252dce38d729c9df3954f3fe66e4e25c8b
WavuaW <wavuamuroka@gmail.com>
2023-05-22 09:39:54 +0300

    unit and intergration testing notes

---
+++ Python-Practice/Redis/Untitled-1.js
@@ -0,0 +1,12 @@
+{
+    first_name: "Jane",
+    last_name: "Doe",
+    memberships: ["mem1", "mem2"],
+    address:{
+        street: "4 main st",
+        city: "Boston"
+    }
+    contacts:[
+        {name: "Brad", relationship: "friend"}
+    ]
+}
