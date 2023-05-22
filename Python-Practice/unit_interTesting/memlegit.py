52cd32b8896848ac4d99fee5abab983133e9a206
WavuaW <wavuamuroka@gmail.com>
2023-05-22 21:47:16 +0300

    memoization

---
+++ Python-Practice/unit_interTesting/memlegit.py
@@ -0,0 +1,18 @@
+from functools import lru_cache
+
+@lru_cache(maxsize=1000)
+def fibonacci(n):
+    #checks if the input is an integer
+    if type(n) != int:
+        raise TypeError("n must be an integer")
+    if n < 1:
+        raise ValueError("n must be a postive integer")
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 1
+    elif n > 2:
+        return fibonacci(n-1) + fibonacci(n-2)
+    
+for n in range(1, 501):
+    print(n, ';', fibonacci(n))
---
+++ Python-Practice/unit_interTesting/memrec.py
@@ -0,0 +1,20 @@
+fibonacci_cache = {}
+
+def fibonacci(n):
+    #caching the value 
+    if n in fibonacci_cache:
+        return fibonacci_cache[n]
+    
+    if n == 1:
+        value = 1
+    elif n == 2:
+        value = 1
+    elif n > 2:
+        value = fibonacci(n-1) + fibonacci(n-2)
+    
+    # Cache the value and return it 
+    fibonacci_cache[n] = value
+    return value
+
+for n in range(1, 101):
+    print(n, ';', fibonacci(n))
