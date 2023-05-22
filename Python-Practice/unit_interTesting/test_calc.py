98984882f061af4f8253b5da4d15c88b05678f46
WavuaW <wavuamuroka@gmail.com>
2023-05-22 12:22:23 +0300

    unit and intergration testing notes

--- Python-Practice/unit_interTesting/test_calc.py
+++ Python-Practice/unit_interTesting/test_calc.py
@@ -4,5 +4,29 @@ import calc
 class TestCalc(unittest.TestCase):
 
     def test_add(self):
-        result = calc.add(10, 6, 10)
-        self.assertEqual(result, 15)
+        self.assertEqual(calc.add(5, 10, 5), 15)
+        self.assertEqual(calc.add(5, -1, 1), 0)
+        self.assertEqual(calc.add(5, -1, -1), -2)
+
+
+    def test_subtract(self):
+        self.assertEqual(calc.subtract(5, 10, 5), 5)
+        self.assertEqual(calc.subtract(5, -1, 1), -2)
+        self.assertEqual(calc.subtract(5, -1, -1), 0)
+
+
+    def test_multiply(self):
+        self.assertEqual(calc.multiply(5, 10, 5), 50)
+        self.assertEqual(calc.multiply(5, -1, 1), -1)
+        self.assertEqual(calc.multiply(5, -1, -1), 1)
+
+
+    def test_divide(self):
+        self.assertEqual(calc.divide(5, 10, 5), 2)
+        self.assertEqual(calc.divide(5, -1, 1), -1)
+        self.assertEqual(calc.divide(5, -1, -1), 1)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
