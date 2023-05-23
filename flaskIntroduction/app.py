e2b824e98776f81d1e93011ed58835d61a785440
WavuaW <wavuamuroka@gmail.com>
2023-05-23 16:15:41 +0300

    POST implementation

--- flaskIntroduction/app.py
+++ flaskIntroduction/app.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from requests import request
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db' # //// - is an absolute path while /// - is relatuve
@@ -11,19 +10,29 @@ db = SQLAlchemy(app)
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<Task %r>' % self.id
 
 @app.route('/', methods=['POST', 'GET'])
-
 def index():
     if request.method == 'POST':
-        pass
+        task_content = request.form['content']
+        new_task = Todo(content, task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
+
     else:
-        render_template('index.html')
-    return render_template('index.html')
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+    
 
 if __name__ == "__main__":
     app.run(port=8080, debug=True)
