4e175fa2b25a891eb8c3ca8e65a04c0d32c9d423
WavuaW <wavuamuroka@gmail.com>
2023-05-25 10:51:59 +0300

    Implementing overwrite and different log messages depending on level

--- Python-Practice/Logging/log1.py
+++ Python-Practice/Logging/log1.py
@@ -4,12 +4,18 @@ import logging
 LOG_FORMAT   = "%(levelname)s %(asctime)s - %(message)s"
 logging.basicConfig( filename = "E:\\python\\Lumberjack.log", 
                     level = logging.DEBUG,
-                    format = LOG_FORMAT)
+                    format = LOG_FORMAT,
+                    filemode = 'w')
 logger = logging.getLogger()
 
 #Test the logger
-logger.info("Our first message")
+logger.info("Our SECOND message after implementting overwrite")
 
 print(logger.level)
 
 #Test Messages
+logger.debug("This is a harmless debug message")
+logger.info("Some useful info")
+logger.warning("I'm sorry, but I can't do that, Dave.")
+logger.error("Did you just try to divide by zero?")
+logger.critical("The entire internet is down!!")
