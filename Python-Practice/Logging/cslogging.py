96be1dcf2a1db5a539a52695728621802034fdbc
WavuaW <wavuamuroka@gmail.com>
2023-05-25 11:45:05 +0300

    simple calculator

--- Python-Practice/Logging/cslogging.py
+++ Python-Practice/Logging/cslogging.py
@@ -14,9 +14,13 @@ num_1 = 10
 num_2 = 5
 
 add_result = add(num_1, num_1)
+print('Add: {} + {} = {}'.format(num_1, num_2, add_result))
 
 sub_result = subrtact(num_1, num_2)
+print('Sub: {} - {} = {}'.format(num_1, num_2, sub_result))
 
 mul_result = multiply(num_1, num_2)
+print('Mul: {} * {} = {}'.format(num_1, num_2, mul_result))
 
 div_result = divide(num_1, num_2)
+print('Div: {} + {} = {}'.format(num_1, num_2, div_result))
