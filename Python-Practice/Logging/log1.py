2567a267f2d5e736e2dff5b9769acb0526be7fcb
WavuaW <wavuamuroka@gmail.com>
2023-05-25 10:47:45 +0300

    logging intoduction

--- Python-Practice/Logging/log1.py
+++ Python-Practice/Logging/log1.py
@@ -1,10 +1,15 @@
 import logging
 
 #Create and configure a logger
-logging.basicConfig( filename = "E:\\python\\Lumberjack.log")
+LOG_FORMAT   = "%(levelname)s %(asctime)s - %(message)s"
+logging.basicConfig( filename = "E:\\python\\Lumberjack.log", 
+                    level = logging.DEBUG,
+                    format = LOG_FORMAT)
 logger = logging.getLogger()
 
 #Test the logger
 logger.info("Our first message")
 
 print(logger.level)
+
+#Test Messages
