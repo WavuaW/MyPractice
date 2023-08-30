2bf623c5f426f9f8ceeebe17cb7dbdbf213778db
WavuaW <wavuamuroka@gmail.com>
2023-08-30 12:18:16 +0300

    Simple steamlit app

---
+++ Streamlit/app.py
@@ -0,0 +1,8 @@
+import streamlit as st 
+
+st.set_page_config(page_title="My Webpage", layout="wide")
+
+st.subheader("Hi, I am Chris :alien_monster:")
+st.title("A Future Data Scientist From Kenya")
+st.write("I am passionate about tech and finding solutions and innovations related to tech")
+st.write("[Learn More >](https://www.webfx.com/tools/emoji-cheat-sheet/)")
