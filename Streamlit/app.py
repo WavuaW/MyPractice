4b4276409291dcc65c8d9117012f69e592f5647f
WavuaW <wavuamuroka@gmail.com>
2023-08-30 21:11:31 +0300

    Finished Simple Web App.

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -15,7 +15,10 @@ def load_lottieurl(url):
         return None
     return r.json()
 
-
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+local_css("style/style.css")
 with st.container():
     st.subheader("Hi, I am Chris :alien_monster:")
     st.title("A Future Data Scientist From Kenya")
