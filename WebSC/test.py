bbbcd1b24a20395486507f6fddfed091ed07462a
WavuaW <wavuamuroka@gmail.com>
2023-08-31 19:14:18 +0300

    Python Autpomation

--- WebSC/test.py
+++ WebSC/test.py
@@ -1,5 +1,8 @@
 import requests
+from bs4 import BeautifulSoup
 
 response = requests.get('https://www.bbc.com/news')
+soup = BeautifulSoup(response.content, 'html.parser')
 
 print(response.status_code)
+print(soup)
