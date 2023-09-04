893bfc3904ead51f68f9f607d2c60af0666655d8
WavuaW <wavuamuroka@gmail.com>
2023-09-04 19:01:34 +0300

    Person Class

--- Python-Practice/object_relational_mapping/book_script.py
+++ Python-Practice/object_relational_mapping/book_script.py
@@ -7,3 +7,19 @@ Base = declarative_base()
 
 class Person(Base):
     __tablename__ = "people"
+
+    ssn = Column("ssn", Integer, primary_key=True)
+    firstname = Column("firstname", String)
+    lastname = Column("lastname", String)
+    gender = Column("gender", CHAR)
+    age = Column("age", Integer)
+
+    def __init__(self, ssn, firstname, lastname, gender, age):
+        self.ssn = ssn
+        self.firstname = firstname
+        self.lastname = lastname
+        self.gender = gender
+        self.age = age
+    
+    def __repr__(self):
+        return f"({self.ssn}) {self.firstname} {self.lastname} ({self.gender}, {self.age})"
