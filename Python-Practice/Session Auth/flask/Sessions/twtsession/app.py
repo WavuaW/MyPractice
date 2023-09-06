fbc62b7194f1994c4618bfb4bc5f898c699d52d0
WavuaW <wavuamuroka@gmail.com>
2023-09-06 10:56:49 +0300

    Basic Flask

--- Python-Practice/Session Auth/flask/Sessions/twtsession/app.py
+++ Python-Practice/Session Auth/flask/Sessions/twtsession/app.py
@@ -1,14 +1,18 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return
+    return "Hello World <h1>TRASH CAN</h1>"
 
-@app.route("/<>")
+@app.route("/<name>")
 def user(name):
     return f"Hello {name}"
+
+@app.route('/admin')
+def admin():
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run()
