9f285ce54d9e366f09883c079e8630e0c3bbd572
WavuaW <wavuamuroka@gmail.com>
2023-09-23 17:40:10 +0300

    how to handle child process errors

--- Streak/Node/0x00/spawn.js
+++ Streak/Node/0x00/spawn.js
@@ -10,3 +10,7 @@ var child = cp.spawn(progs.list, ["-a"], {cwd:".."});
 child.stdout.on ('data', (data) => {
   console.log(`data\n${data}`)  
 });
+
+child.stderr.on('data', (err) => {
+    console.log(`error\n${err}`)
+})
