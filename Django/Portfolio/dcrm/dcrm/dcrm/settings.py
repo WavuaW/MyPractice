4676b3204bec8da6cfd28cf140b2f8b41b44185e
WavuaW <wavuamuroka@gmail.com>
2023-10-02 21:41:35 +0300

    Setting up mysql database

--- Django/Portfolio/dcrm/dcrm/dcrm/settings.py
+++ Django/Portfolio/dcrm/dcrm/dcrm/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'dcrm.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': BASE_DIR,
+        'NAME': 'chrisco',
         'USER': 'root',
         'PASSWORD': 'password123',
         'HOST': 'localhost',
