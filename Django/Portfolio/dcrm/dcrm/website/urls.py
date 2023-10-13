a1d56a53982ed370ca5ad4fe3152ffa49a1f4c18
WavuaW <wavuamuroka@gmail.com>
2023-10-13 23:45:53 +0300

    Login authentication for Admin

--- Django/Portfolio/dcrm/dcrm/website/urls.py
+++ Django/Portfolio/dcrm/dcrm/website/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     # path('', views.login_user, name='login'),
-    # path('', views.logout_user, name='logout'),
+    path('', views.logout_user, name='logout'),
+    
 ]
